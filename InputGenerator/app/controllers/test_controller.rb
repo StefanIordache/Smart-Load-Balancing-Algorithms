53de1db21f862070b61b712543ed38d21164889d
StefanIordache <iordachestefan1@gmail.com>
2019-03-27 00:09:17 +0200

    Controller demo for python integration

--- InputGenerator/app/controllers/test_controller.rb
+++ InputGenerator/app/controllers/test_controller.rb
@@ -8,7 +8,20 @@ class TestController < ApplicationController
 
     def simulate
         logger.info params[:payload]
-        raise "baaam"
+
+        # (0..2).each do
+        #     slave = IO.popen(['python', File.expand_path('../Test/slave.py')], mode='r+')
+        #     slave.write "master"
+        #     slave.close_write
+        #     line = slave.readline
+        #     while line do
+        #       sleep 1
+        #       p eval line
+        #       break if slave.eof
+        #       line = slave.readline
+        #     end
+        #   end
+
     end
 
 end
