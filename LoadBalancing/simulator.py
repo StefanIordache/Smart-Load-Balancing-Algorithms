b71219f77b68689060aa23dc2b467155a99618d8
StefanIordache <iordachestefan1@gmail.com>
2019-04-02 12:48:59 +0300

    Pipe prototype completed

--- LoadBalancing/simulator.py
+++ LoadBalancing/simulator.py
@@ -4,44 +4,25 @@ import time
 
 if __name__ == "__main__":
     logger = open ("log.txt", "w+")
+    log1 = open ("log1.txt", "w+")
+    log2 = open ("log2.txt", "w+")
 
+    info_cluster = sys.stdin.readline()
+    logger.write(str(info_cluster))
 
-    reader = sys.stdin.read()
-    info_cluster = reader
-
-    logger.write("am citit info_cluster")
-
-    f = open ("cluster.json", "w+")
-    f.write(info_cluster)
-    f.close()
-
-    logger.writelines("am realizat cluster.json")
+    logger.write("am citit info_cluster\n")
     
-    print "OK1\n"
-    print '\0'
+    print "OK1"
     sys.stdout.flush()
 
-    logger.writelines("am afisat OK1")
+    logger.write("am afisat OK1\n")
 
-    reader = sys.stdin.read()
-    info_jobs = reader
+    info_jobs = sys.stdin.readline()
+    logger.write(str(info_jobs))
 
-    logger.writelines("am citit info_jobs")
-
-    g = open ("jobs.json", "w+")
-    g.write(info_jobs)
-    g.close()
-
-    logger.writelines("am realizat jobs.json")
+    logger.write("am citit info_jobs\n")
 
     print "OK2"
     sys.stdout.flush()
 
-    logger.writelines("am afisat OK2")
-
-    """ while reader:
-        x = reader
-        for i in range(0, 5):
-            print "{'%i'=>'%s'}" % (i, x)
-            sys.stdout.flush()
-            reader = sys.stdin.read() """
+    logger.write("am afisat OK2\n")
--- TestEnvironment/app/helpers/algorithm_simulator_helper.rb
+++ TestEnvironment/app/helpers/algorithm_simulator_helper.rb
@@ -3,120 +3,24 @@ module AlgorithmSimulatorHelper
   require 'open3'
   require 'expect'
   require 'io/wait'
+  require 'io/console'
+  require 'socket'
 
   def run_simulation_script(info_cluster, info_jobs, script_path)
 
     puts script_path
-    # Open3.popen3(['python', File.expand_path('../LoadBalancing/simulator.py')], mode='rw+') do  |stdin, stdout, stderr|
-    #   stdout.close_write
-    # end
 
-=begin
-    IO.popen(['python', script_path], mode='r+') do |pipe|
-      pipe.puts info_cluster
-      pipe.close_write
-
-      result_cluster = pipe.gets
-
-      puts result_cluster
-
-      pipe.puts info_jobs
-      pipe.close_write
-
-      result_jobs = pipe.gets
-
-
-      puts result_jobs
-
-    end
-=end
-
-    Open3.popen3(['python', script_path]) do |stdin, stdout, stderr|
-
-      stdin.puts info_cluster
-
-      while line =  stdout.gets
-        puts line
-      end
-
-      stdin.puts info_jobs
-=begin
-      result_jobs = stdout.read
-
-      puts result_jobs
-=end
-    end
-
-
-=begin
     slave = IO.popen(['python', script_path], mode = 'r+')
 
-    # slave.write info_cluster
     slave.puts info_cluster
-    slave.clo
 
     line = slave.readline
     puts line
 
-    slave.write info_jobs
-    slave.close_write
+    slave.puts info_jobs
 
     line = slave.readline
     puts line
-=end
-
-=begin
-    Open3.popen3(['python', script_path]) do | input, output, error, wait_thr|
-      input.sync = true
-      output.sync = true
-
-      input.write info_cluster
-
-      while output.ready?
-        puts output.readline
-      end
-      # puts output.expect("OK1", 4)
-
-      input.write info_jobs
-      while output.ready?
-        puts output.readline
-      end
-      # puts output.expect("OK2", 4)
-
-    end
-=end
-
-=begin
-    stdin, stdout, stderr = Open3.popen3(['python', script_path])
-
-    stdin.puts info_cluster
-
-    puts "1"
-
-    while stdout.ready?
-      puts stdout.readline
-    end
-
-    puts "2"
-
-    stdin.puts info_jobs
-
-    puts "3"
-
-    while stdout.ready?
-      puts stdout.readline
-    end
-
-    puts "4"
-=end
-
-=begin
-    Open3.popen3("sleep 2; ls") do |stdout, stderr, status, thread|
-      while line=stderr.gets do
-        puts(line)
-      end
-    end
-=end
 
 
   end
